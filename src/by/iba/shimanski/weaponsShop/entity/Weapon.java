a65f2c711022a69d899259ccc3dd38db870bce4c
Vlad Shimanski <vladshimanski@gmail.com>
2016-02-25 10:55:05 +0300

    add hashCode and equals

--- src/by/iba/shimanski/weaponsShop/entity/Weapon.java
+++ src/by/iba/shimanski/weaponsShop/entity/Weapon.java
@@ -55,4 +55,43 @@ public abstract class Weapon {
 					+ producer + "]";
 		}
 
+		
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result
+					+ ((damage == null) ? 0 : damage.hashCode());
+			result = prime * result
+					+ ((producer == null) ? 0 : producer.hashCode());
+			result = prime * result + ((size == null) ? 0 : size.hashCode());
+			return result;
+		}
+
+		
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Weapon other = (Weapon) obj;
+			if (damage == null) {
+				if (other.damage != null)
+					return false;
+			} else if (!damage.equals(other.damage))
+				return false;
+			if (producer != other.producer)
+				return false;
+			if (size == null) {
+				if (other.size != null)
+					return false;
+			} else if (!size.equals(other.size))
+				return false;
+			return true;
+		}
+
+		
 }
--- src/by/iba/shimanski/weaponsShop/shop/Goods.java
+++ src/by/iba/shimanski/weaponsShop/shop/Goods.java
@@ -37,5 +37,36 @@ public class Goods<T extends Weapon> {
 		this.productCost = productCost;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((product == null) ? 0 : product.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(productCost);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Goods other = (Goods) obj;
+		if (product == null) {
+			if (other.product != null)
+				return false;
+		} else if (!product.equals(other.product))
+			return false;
+		if (Double.doubleToLongBits(productCost) != Double
+				.doubleToLongBits(other.productCost))
+			return false;
+		return true;
+	}
+
 	
 }
